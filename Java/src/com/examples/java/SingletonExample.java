c4d7811eac6139b0a61db3ee7da3ca2b488d17ae
karthickgunasekar <569gkarthi@gmail.com>
2018-01-03 14:29:55 +0530

    Initial Commit

---
+++ Java/src/com/examples/java/SingletonExample.java
@@ -0,0 +1,14 @@
+package com.examples.java;
+
+public class SingletonExample {
+	private static SingletonExample singletonExample = null;
+	public String str;
+	private SingletonExample (){
+		str = "Singleton Class Created";
+	}
+	public static SingletonExample getInstance(){
+		if(singletonExample==null)
+			singletonExample = new SingletonExample();
+		return singletonExample;
+	}
+}
---
+++ Java/src/com/examples/java/SingletonMainExample.java
@@ -0,0 +1,18 @@
+package com.examples.java;
+
+public class SingletonMainExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingletonExample singletonExample1 = SingletonExample.getInstance();
+		singletonExample1.str = (singletonExample1.str).toLowerCase();
+		System.out.println(singletonExample1.str);
+		
+		SingletonExample singletonExample2 = SingletonExample.getInstance();
+		singletonExample2.str = (singletonExample2.str).toUpperCase();
+		System.out.println(singletonExample2.str);
+		
+		//SingletonExample singletonExample3 = new SingletonExample();
+	}
+
+}

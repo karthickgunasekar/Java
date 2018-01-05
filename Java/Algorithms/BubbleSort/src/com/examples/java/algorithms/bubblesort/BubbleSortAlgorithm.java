372260037589620187af3f026371c3a5d0b7302c
karthickgunasekar <569gkarthi@gmail.com>
2018-01-05 13:20:08 +0530

    Completed src

--- Java/Algorithms/BubbleSort/src/com/examples/java/algorithms/bubblesort/BubbleSortAlgorithm.java
+++ Java/Algorithms/BubbleSort/src/com/examples/java/algorithms/bubblesort/BubbleSortAlgorithm.java
@@ -1,5 +1,36 @@
 package com.examples.java.algorithms.bubblesort;
 
-public class BubbleSortAlgorithm {
+import java.util.Comparator;
 
+public class BubbleSortAlgorithm {
+	public static void BS(Integer[] array, int len) {
+		boolean swap;
+		do {
+			swap = false;
+			// 569237184
+			for (int count = 0; count < len - 1; count++) {
+				int comp = array[count].compareTo(array[count + 1]);
+				if (comp > 0) {
+					int temp = array[count];
+					array[count] = array[count + 1];
+					array[count + 1] = temp;
+					swap = true;
+				}
+			}
+			len--;
+		} while (swap);
+		for (int curArray : array)
+			System.out.println(curArray);
+	}
+
+	public static void main(String[] args) {
+		// 569237184
+		int[] numbers = { 5, 6, 9, 2, 3, 7, 1, 8, 4 };
+		int valLen = numbers.length;
+		Integer[] intClassNum = new Integer[valLen];
+		for (int i = 0; i < valLen; i++) {
+			intClassNum[i] = numbers[i];
+		}
+		BS(intClassNum, valLen);
+	}
 }
